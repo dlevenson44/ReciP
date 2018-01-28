e04a4ac663186ace5a66354138e2cf91b94ca486
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-28 17:58:00 -0500

    set up recipe-helper

---
+++ services/recipes/recipe-helper.js
@@ -0,0 +1,28 @@
+require('isomorphic-fetch')
+
+function getRecipes(req, res, next) {
+	fetch('https://api.edamam.com/search', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			api_id: process.env.APP_ID,
+			app_key: process.env.APP_KEY,
+			data: 'this is a test'
+		})
+	}).then(fetchRes => fetchRes.json())
+	.then(jsonRes => {
+		console.log(jsonRes)
+		next()
+	}).catch(err => {
+		console.log(err)
+		next(err)
+	})
+}
+
+module.exports = {
+	getRecipes: getRecipes,
+}
+
+
