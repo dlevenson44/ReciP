c9e3957ae1b701d9c6cdb827e3728c4ece7d2312
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-16 14:49:04 -0500

    adding health, ingredient, link, and image info

--- public/src/main.js
+++ public/src/main.js
@@ -11,30 +11,42 @@ function getRecipes(e) {
         for (let i = 0; i < recipeArray.length; i++) {
             // set API data to variables
             let dietLabels = recipeArray[i].recipe.dietLabels
-            // let healthLabels = recipeArray[i].recipe.healthLabels
-            // let ingredients = recipeArray[i].recipe.ingredients
+            let healthLabels = recipeArray[i].recipe.healthLabels
+            let ingredients = recipeArray[i].recipe.ingredients
             let recipeName = recipeArray[i].recipe.label
             let recipeDiet = recipeArray[i].recipe.dietLabels[0]
             let recipeCalories = recipeArray[i].recipe.calories
             let recipeServings = recipeArray[i].recipe.yield
-            // let recipeHealth = recipeArray[i].recipe.healthLabels[0]
-            // let recipeIngredients = recipeArray[i].recipe.ingredients[0]
+            let recipeHealth = recipeArray[i].recipe.healthLabels[0]
+            let recipeIngredients = recipeArray[i].recipe.ingredients[0].text
+            let recipeLink = recipeArray[i].recipe.shareAs
+            let recipeImg = recipeArray[i].recipe.image
 
             // create HTML attributes
             let title = document.createElement('h1')
             let diet = document.createElement('p')
             let calories = document.createElement('p')
-            // let servings = document.createElement('p')   
-            // let health = document.createElement('p')
-            // let ingredients = document.createAttribute('p')         
+            let servings = document.createElement('p')   
+            let health = document.createElement('p')
+            let ingredient = document.createElement('p')
+            let link = document.createElement('a') 
+            let img = document.createElement('img')    
 
-            // set attributes
+            // set classes
             title.setAttribute('class', 'recipe-name')
             diet.setAttribute('class', 'recipe-diet')
             calories.setAttribute('class', 'recipe-calories')
-            // servings.setAttribute('class', 'recipe-servings')
-            // health.setAttribute('class', 'recipe-health')
-            // ingredients.setAttribute('class', 'recipe-ingredient')
+            servings.setAttribute('class', 'recipe-servings')
+            health.setAttribute('class', 'recipe-health')
+            ingredient.setAttribute('class', 'recipe-ingredient')
+            link.setAttribute('class', 'recipe-link')
+            img.setAttribute('class', 'recipe-img')
+
+            // set link URL
+            link.setAttribute('href', recipeLink)
+
+            // set img source
+            img.setAttribute('src', recipeImg)
             
             // convert calories to calories per serving
             calPerServing = (recipeCalories/recipeServings).toPrecision(4)
@@ -42,9 +54,10 @@ function getRecipes(e) {
             // set innerHTML
             title.innerHTML = recipeName
             calories.innerHTML = calPerServing + ' calories per serving'
-            // servings.innerHTML = recipeServings + ' servings'
-            // health.innerHTML = recipeHealth
-            // ingredients.innerHTML = recipeIngredients
+            servings.innerHTML = recipeServings + ' servings'
+            health.innerHTML = recipeHealth
+            ingredient.innerHTML = recipeIngredients
+            link.innerHTML = 'Click here for the full recipe'
 
             // only append diet label if available, display diet values
             if (dietLabels.length >= 1) {
@@ -54,7 +67,7 @@ function getRecipes(e) {
             }
 
             // append to container
-            $(".container").append(title, diet, calories)
+            $(".container").append(title, diet, calories, servings, health, ingredient, link, img)
         }
     })
 }
