6644e182f138b7de7e3e9c8ebb212a91afac4038
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-16 13:30:52 -0500

    added DOM manipulation to display recipe name and diet

--- public/src/main.js
+++ public/src/main.js
@@ -5,7 +5,29 @@ function getRecipes(e) {
     fetch(`/recipe/${e.target.search.value}`)
     .then(res => res.json())
     .then(res => {
-        console.log(res)
+        console.log(res)        
+        let recipeArray = res.recipe.hits
+        $(".recipe-name").empty()
+        for (let i = 0; i < recipeArray.length; i++) {
+            // set API data to variables
+            let recipeName = recipeArray[i].recipe.label
+            let recipeDiet = recipeArray[i].recipe.dietLabels[0]            
+
+            // create HTML attributes
+            let title = document.createElement('h1')
+            let diet = document.createElement('p')
+
+            // set attributes
+            title.setAttribute('class', 'recipe-name')
+            diet.setAttribute('class', 'recipe-diet')
+            
+            // set innerHTML
+            title.innerHTML = recipeName
+            diet.innerHTML = recipeDiet
+
+            // append to container
+            $(".container").append(title, diet)
+        }
     })
 }
 
