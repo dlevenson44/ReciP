d2bb138d3de9b9376ebea1e50e6c3c46d31e3198
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-19 04:20:33 -0500

    fix model to link user and favorite entries

--- models/recipe.js
+++ models/recipe.js
@@ -23,7 +23,7 @@ Recipe.create = (recipe, userid) => {
 		(title, link, img, diet, user_id)
 		VALUES ($1, $2, $3, $4, $5)
 		RETURNING *
-		`, [recipe.title, recipe.link, recipe.img, recipe.diet, userid])
+		`, [recipe.title, recipe.link, recipe.img, recipe.diet, recipe.user_id, userid])
 }
 
 Recipe.update = (recipe, id) => {
