1d9ccb0e905c563f6c22e00359f80fa4da9af9e8
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-28 14:58:50 -0500

    changed number of results per search from 25 to 30

--- services/recipes/recipe-helpers.js
+++ services/recipes/recipe-helpers.js
@@ -2,7 +2,7 @@ require('isomorphic-fetch')
 require('dotenv').config()
 
 function getRecipes(req, res, next) {
-	fetch(`https://api.edamam.com/search?q=${req.params.search}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=25`)
+	fetch(`https://api.edamam.com/search?q=${req.params.search}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=30`)
 	.then(res => res.json())
 	// use res.locals to attach data to repsonse object
 	.then(fetchRes => {
